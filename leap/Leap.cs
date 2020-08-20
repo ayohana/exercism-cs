60b5f16f3bbe9431c7a16f59c5dd288538bf8f6b
Adela Darmansyah <adela.yohana@gmail.com>
2020-08-20 14:54:40 -0700

    Refactor conditionals in determining leap year

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -4,20 +4,15 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        if (year % 100 == 0 && year % 400 == 0)
-        {
-            return true;
-        }
-        else if (year % 100 == 0 && year % 400 != 0)
+        if (year % 4 != 0)
         {
             return false;
         }
-
-        if (year % 4 == 0)
+        if (year % 100 == 0 && year % 400 != 0)
         {
-            return true;
-        }
-        
-        return false;
+            return false;
+        }  
+              
+        return true;
     }
 }
