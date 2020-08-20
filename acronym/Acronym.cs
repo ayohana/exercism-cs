86d2a86e5659e19df65b2a6f6727757402caea05
Adela Darmansyah <adela.yohana@gmail.com>
2020-08-20 11:36:22 -0700

    Refresh on integers, conditionals, DateTime objects, TimeSpan

---
+++ acronym/Acronym.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        string output = "";
+        char[] delimiters = new char[] { ' ', ',', '-', '_' };
+        string[] words = phrase.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+        for (int i = 0; i < words.Length; i++)
+        {
+            output = output + words[i][0];
+        }
+
+        return output.ToUpper();
+    }
+}
